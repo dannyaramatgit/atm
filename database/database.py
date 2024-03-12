6417855445a0f9c94a3a49bcb539ad2f58eefbe7
Danny Aram <dannyaram@gmail.com>
2024-03-12 02:50:14 +0200

    added postgres configuration

--- database/database.py
+++ database/database.py
@@ -2,16 +2,23 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import URL
 from database.models import Cash, Base
 
 logger = logging.getLogger(__name__)
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# SQLALCHEMY_DATABASE_URL = URL.create(
+#     drivername="postgresql",
+#     username="postgres",
+#     password="admin123",
+#     host="localhost",
+#     database="postgres",
+#     port=5432
+# )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
